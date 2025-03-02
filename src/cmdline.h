d0a41c69e75f5b4aaae0d16e411fd23e3d9a8f92
Bryan Rykowski <90996715+BryanRykowski@users.noreply.github.com>
2025-03-02 17:29:00 -0500

    Add cmdline.h
    cmdline.h adds 3 functions.
    
    argWindowSize() handles the arg "--window-size=widthxheight", providing
    the extracted width and height.
    
    argFullscreen() handles the arg "--fullscreen", returning true if it is
    found..
    
    argLevelName() returns the first arg not starting with "--".

---
+++ src/cmdline.h
@@ -0,0 +1,66 @@
+#ifndef H_CMDLINE
+#define H_CMDLINE
+
+#include <string>
+#include <stdlib.h>
+
+int argLevelName(int argc, char** argv) {
+    std::string arg;
+
+    for (int i = 1; i < argc; ++i) {
+        arg = argv[i];
+
+        if ((arg.size() >= 1) && arg[0] == '-') continue;
+        return i;
+    }
+
+    return 0;
+}
+
+template <typename T>
+bool argWindowSize(int argc, char** argv, T* w, T* h) {
+    std::string arg = "";
+    std::string sub = "";
+
+    for (int i = 1; i < argc; ++i) {
+        arg = argv[i];
+
+        if (arg.size() > 14 && arg.substr(0, 14) == "--window-size=") {
+            sub = arg.substr(arg.find('=') + 1, arg.size());
+            break;
+        }
+    }
+
+    if (sub.empty()) return false;
+
+    size_t pos;
+    pos = sub.find("x");
+    if (pos == sub.npos) return false;
+    if (pos + 1 >= sub.size()) return false;
+
+    std::string w_str = sub.substr(0, pos);
+    std::string h_str = sub.substr(pos + 1, sub.size());
+
+    unsigned int w_tmp = atoi(w_str.c_str());
+    unsigned int h_tmp = atoi(h_str.c_str());
+    if (w_tmp == 0 || h_tmp == 0) return false;
+
+    if (w != 0) *w = w_tmp;
+    if (h != 0) *h = h_tmp;
+
+    return true;
+}
+
+bool argFullscreen(int argc, char** argv) {
+    std::string arg;
+
+    for (int i = 1; i < argc; ++i) {
+        arg = argv[i];
+
+        if (arg == "--fullscreen") return true;
+    }
+
+    return false;
+}
+
+#endif
